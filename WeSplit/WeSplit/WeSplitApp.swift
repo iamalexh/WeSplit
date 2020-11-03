8de0006fabd5e23faa21b3d2ad63e7fba6060466
iamalexh <67149386+iamalexh@users.noreply.github.com>
2020-11-03 13:48:11 +0000

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alex on 29/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
